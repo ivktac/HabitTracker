a10127d5da1f30348e15dce56495b45f1e298323
Ivan Tkachuk <ivktac@gmail.com>
2024-04-24 00:25:49 +0300

    Add more get current user

--- HabitTracker/Services/HabitService.cs
+++ HabitTracker/Services/HabitService.cs
@@ -67,9 +67,15 @@ public class HabitService(ApplicationDbContext context, AuthenticationStateProvi
 
     public async Task<Habit> GetHabitByIdAsync(Guid id)
     {
+        var userId = await GetUserIdAsync();
+        if (string.IsNullOrEmpty(userId)) {
+            throw new Exception("User not found");
+        }
+
         return await context.Habits
             .Include(h => h.Color)
             .Include(h => h.Frequencies)
+            .Where(h => h.UserId == userId)
             .FirstOrDefaultAsync(h => h.Id == id) ?? throw new Exception("Habit not found");
 
     }
@@ -92,15 +98,38 @@ public class HabitService(ApplicationDbContext context, AuthenticationStateProvi
 
     public async Task<List<Habit>> GetUncompletedHabitsAsync()
     {
+        var userId = await GetUserIdAsync();
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            throw new Exception("User not found");
+        }
+
         return await context.Habits
             .Include(h => h.Color)
             .Include(h => h.Frequencies)
+            .Where(h => h.UserId == userId)
             .Where(h => !h.IsCompleted)
             .ToListAsync();
     }
 
     public async Task UpdateHabitAsync(Habit habit)
     {
+        var userId = await GetUserIdAsync();
+        if (string.IsNullOrEmpty(userId)) {
+            throw new Exception("User not found");
+        }
+
+        if (habit.UserId != userId) {
+            throw new Exception("Habit not found");
+        }
+
+        if (!habit.IsCompleted) {
+            habit.EndDate = DateTime.Now;
+        }
+
+        habit.UserId = userId;
+
         var entry = context.Habits.Entry(habit);
         if (entry.State == EntityState.Detached)
         {
