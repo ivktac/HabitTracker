afedac6930d29dee26067609eef2409da4a1c97c
Ivan Tkachuk <ivktac@gmail.com>
2024-06-05 01:02:18 +0300

    Add shared state

---
+++ HabitTracker/Services/HabitStateService.cs
@@ -0,0 +1,18 @@
+﻿using HabitTracker.Data.Entities;
+
+namespace HabitTracker.Services;
+
+public class HabitStateService
+{
+    public event Action OnChange;
+
+    public List<Habit> Habits { get; private set; } = [];
+
+    public void UpdateHabits(List<Habit> habits)
+    {
+        Habits = habits;
+        NotifyStateChanged();
+    }
+
+    public void NotifyStateChanged() => OnChange?.Invoke();
+}
